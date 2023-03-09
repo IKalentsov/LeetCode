c64b5f7c1dd2cf649042f0f33da0067834033fc3
ProjectHW <ya.ko4erapbuhunyx@yandex.ru>
2023-03-09 22:02:33 +0300

    Решил задачу 412

---
+++ LeetCodeSolutions/LeetCodeSolutions/412_FizzBuzz.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    /// <summary>
+    /// Given an integer n, return a string array answer 
+    /// (1-indexed) where:
+
+    /// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+    /// answer[i] == "Fizz" if i is divisible by 3.
+    /// answer[i] == "Buzz" if i is divisible by 5.
+    /// answer[i] == i(as a string) if none of the above conditions
+    /// are true.
+    /// </summary>
+    public class FizzBuzz
+    {
+        private const string fizzBuzz = "FizzBuzz";
+        private const string fizz = "Fizz";
+        private const string buzz = "Buzz";
+
+        public static IList<string> FizzBuzzSolution(int n)
+        {
+            var result = new List<string>();
+
+            for(var i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) result.Add(fizzBuzz);
+
+                else if (i % 3 == 0) result.Add(fizz);
+
+                else if (i % 5 == 0) result.Add(buzz);
+
+                else result.Add(i.ToString());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolutions/LeetCodeTests/UT_412_Fizz_Buzz.cs
@@ -0,0 +1,52 @@
+﻿using LeetCodeSolutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTests
+{
+    public class UT_412_Fizz_Buzz
+    {
+        [Test]
+        public void TestSet1()
+        {
+            var n = 3;
+
+            var result = new List<string>()
+            {
+                "1","2","Fizz"
+            };
+
+            Assert.That(result, Is.EqualTo(FizzBuzz.FizzBuzzSolution(n)));
+        }
+
+        [Test]
+        public void TestSet2()
+        {
+            var n = 5;
+
+            var result = new List<string>() 
+            {
+                "1", "2", "Fizz", "4", "Buzz"
+            };
+
+            Assert.That(result, Is.EqualTo(FizzBuzz.FizzBuzzSolution(n)));
+        }
+
+        [Test]
+        public void TestSet3()
+        {
+            var n = 15;
+
+            var result = new List<string>()
+            {
+                "1","2","Fizz","4","Buzz","Fizz","7","8","Fizz",
+                "Buzz","11","Fizz","13","14","FizzBuzz"
+            };
+
+            Assert.That(result, Is.EqualTo(FizzBuzz.FizzBuzzSolution(n)));
+        }
+    }
+}
