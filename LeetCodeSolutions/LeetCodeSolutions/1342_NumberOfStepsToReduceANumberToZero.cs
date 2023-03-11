80f4b37fa82a223ed9a437ad0714d39db47b63b2
ProjectHW <ya.ko4erapbuhunyx@yandex.ru>
2023-03-11 15:53:37 +0300

    Решил задачу 1342

---
+++ LeetCodeSolutions/LeetCodeSolutions/1342_NumberOfStepsToReduceANumberToZero.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCodeSolutions
+{
+    /// <summary>
+    /// 1342. Number of Steps to Reduce a Number to Zero
+    /// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+    /// 
+    /// Description
+    /// Given an integer num, return the number of steps to reduce it to zero.
+    /// In one step, if the current number is even, you have to divide it by 2,
+    /// otherwise, you have to subtract 1 from it.
+    /// </summary>
+    public class NumberOfStepsToReduceANumberToZero
+    {
+        public static int steps = 0;
+
+        public static int NumberOfSteps(int num)
+        {
+            steps = 0;
+
+            CalculateSteps(num);
+
+            return steps;
+        }
+
+        private static int CalculateSteps(int num)
+        {
+            if (num == 0) return steps;
+
+            steps++;
+
+            if (num % 2 == 0)
+            {
+                num /= 2;
+
+                return CalculateSteps(num);
+            }
+            else
+            {
+                num -= 1;
+                return CalculateSteps(num);
+            }
+        }
+    }
+}
---
+++ LeetCodeSolutions/LeetCodeTests/UT_1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.cs
@@ -0,0 +1,37 @@
+﻿using LeetCodeSolutions;
+
+namespace LeetCodeTests
+{
+    public class UT_1342_Number_of_Steps_to_Reduce_a_Number_to_Zero
+    {
+        [Test]
+        public void TestSet1()
+        {
+            var num = 14;
+
+            var outNums = 6;
+
+            Assert.That(outNums, Is.EqualTo(NumberOfStepsToReduceANumberToZero.NumberOfSteps(num)));
+        }
+
+        [Test]
+        public void TestSet2()
+        {
+            var num = 8;
+
+            var outNums = 4;
+
+            Assert.That(outNums, Is.EqualTo(NumberOfStepsToReduceANumberToZero.NumberOfSteps(num)));
+        }
+
+        [Test]
+        public void TestSet3()
+        {
+            var num = 123;
+
+            var outNums = 12;
+
+            Assert.That(outNums, Is.EqualTo(NumberOfStepsToReduceANumberToZero.NumberOfSteps(num)));
+        }
+    }
+}
