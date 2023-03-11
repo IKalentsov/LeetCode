d1a76809dae18a29a753dd01c9d1f277e94c641b
ProjectHW <ya.ko4erapbuhunyx@yandex.ru>
2023-03-11 15:55:15 +0300

    Добавил ссылки на задачи

--- LeetCodeSolutions/LeetCodeSolutions/412_FizzBuzz.cs
+++ LeetCodeSolutions/LeetCodeSolutions/412_FizzBuzz.cs
@@ -1,9 +1,13 @@
 ﻿namespace LeetCodeSolutions
 {
     /// <summary>
+    /// 412. Fizz Buzz
+    /// https://leetcode.com/problems/fizz-buzz/
+    ///
+    /// Description
     /// Given an integer n, return a string array answer 
     /// (1-indexed) where:
-
+    /// 
     /// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
     /// answer[i] == "Fizz" if i is divisible by 3.
     /// answer[i] == "Buzz" if i is divisible by 5.
--- LeetCodeSolutions/LeetCodeTests/UT_1672_Richest_Customer_Wealth.cs
+++ LeetCodeSolutions/LeetCodeTests/UT_1672_Richest_Customer_Wealth.cs
@@ -2,15 +2,6 @@
 
 namespace LeetCodeTests
 {
-    /// <summary>
-    /// You are given an m x n integer grid accounts where accounts[i][j] 
-    /// is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank.
-    /// Return the wealth that the richest customer has.
-
-    ///A customer's wealth is the amount of money they have in all
-    ///their bank accounts. The richest customer is the customer that
-    ///has the maximum wealth.
-    /// </summary>
     public class UT_1672_Richest_Customer_Wealth
     {
         [Test]
