66cbb316dbe36fabcc284d09efebe1de992c94a2
ProjectHW <ya.ko4erapbuhunyx@yandex.ru>
2025-03-12 20:02:48 +0300

    Добавил тест для задачи 876

---
+++ LeetCodeSolutions/LeetCodeSolutions/876_MiddleOfTheLinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LeetCodeSolutions
+{
+    /// <summary>
+    /// 876. Middle of the Linked List
+    /// https://leetcode.com/problems/middle-of-the-linked-list/
+    /// 
+    /// Description
+    /// Given the head of a singly linked list, return the middle node of 
+    /// the linked list.
+    /// If there are two middle nodes, return the second middle node.
+    /// </summary>
+    public class MiddleOfTheLinkedList
+    {
+        public static ListNode MiddleNode(ListNode head)
+        {
+            var list = new List<ListNode>();
+            var count = 0;
+
+            var next = head;
+            while (next != null)
+            {
+                list.Add(next);
+                count++;
+                next = next.next;
+            }
+
+            return list[count / 2];
+        }
+    }
+
+    public class ListNode
+    {
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+        public int val;
+        public ListNode next;
+    }
+}
---
+++ LeetCodeSolutions/LeetCodeTests/UT_876_Middle_of_the_Linked_List.cs
@@ -0,0 +1,66 @@
+﻿using LeetCodeSolutions;
+
+namespace LeetCodeTests
+{
+    public class UT_876_Middle_of_the_Linked_List
+    {
+        [Test]
+        public void Test_MiddleNode_OddNumberOfNodes()
+        {
+            // Arrange
+            var head = CreateListNode (new List<int> { 1, 2, 3, 4, 5 });
+            var expected = CreateListNode (new List<int> { 3, 4, 5 });
+
+            // Act
+            var result = MiddleOfTheLinkedList.MiddleNode (head);
+
+            // Assert
+            Assert.IsTrue (AreEqual (result, expected));
+        }
+
+        [Test]
+        public void Test_MiddleNode_EvenNumberOfNodes()
+        {
+            // Arrange
+            var head = CreateListNode (new List<int> { 1, 2, 3, 4, 5, 6 });
+            var expected = CreateListNode (new List<int> { 4, 5, 6 });
+
+            // Act
+            var result = MiddleOfTheLinkedList.MiddleNode (head);
+
+            // Assert
+            Assert.IsTrue (AreEqual (result, expected));
+        }
+
+        private ListNode CreateListNode(List<int> values)
+        {
+            if ( values == null || values.Count == 0 )
+                return null;
+
+            var head = new ListNode (values[ 0 ]);
+            var current = head;
+
+            for ( int i = 1; i < values.Count; i++ )
+            {
+                current.next = new ListNode (values[ i ]);
+                current = current.next;
+            }
+
+            return head;
+        }
+
+        private bool AreEqual(ListNode node1, ListNode node2)
+        {
+            while ( node1 != null && node2 != null )
+            {
+                if ( node1.val != node2.val )
+                    return false;
+
+                node1 = node1.next;
+                node2 = node2.next;
+            }
+
+            return node1 == null && node2 == null;
+        }
+    }
+}
